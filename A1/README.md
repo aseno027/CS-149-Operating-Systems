0abc178b19dc178bfff8a4ddff517f86bdc863fd
abelseno <abel.seno@sjsu.edu>
2023-09-09 14:22:22 -0700

    added comments and completed README.md file

--- A1/README.md
+++ A1/README.md
@@ -1,7 +1,31 @@
-/**
- * Description: This module implements matrix operations on matrices and vectors.... It returns a vector...
- * Author names: Abel Seno & Amirali Marsahifar
- * Author emails: abel.seno@sjsu.edu & 
- * Last modified date:9/7/2023
- * Creation date: 9/4/2023
- **/
+Assignment 1 FA23
+
+* Description: This module performs matrix multiplication and addition. Given input matrices A, W, and B, it calculates R = (A * W + B) and prints the resulting matrix R. The dimensions of matrices are fixed: A is 1x3, W is 3x5, and B is 1x5.
+* Author names: Abel Seno & Amirali Marsahifar
+* Author emails: abel.seno@sjsu.edu & amirali.marashifar@sjsu.edu
+* Last modified date: 9/7/2023
+* Creation date: 8/31/2023
+
+Code:
+
+* To compile the program: gcc -o matrixmult matrixmult.c -Wall -Werror
+
+Test Cases:
+
+* Test cases are located in the A1/test directory.
+* To run them, use the following command format: ./matrixmult test/[TestFileNameA] test/[TestFileNameW] test/[TestFileNameB]
+
+Expected Outputs:
+
+Test Case 1:
+Input: ./matrixmult A1.txt W1.txt B1.txt
+Expected Output: Result of A*W+B = [ 111 191 391 51 11 ]
+
+Test Case 2:
+Input: ./matrixmult A2.txt W2.txt B2.txt
+Expected Output: Result of A2*W2+B2 = [ 701 151 191 51 11 ]
+
+Test Case 3:
+Input: ./matrixmult A3.txt W3.txt B3.txt
+Expected Output: Result of A3*W3+B3 = [ 11 19 39 55 55 ]
+
--- A1/matrixmult.c
+++ A1/matrixmult.c
@@ -13,19 +13,20 @@ int readMatrixFile(const char *filename, int numRows, int numCols, int matrix[nu
     if(fp == NULL){
         fprintf(stderr, "error: cannot open file %s\n", filename);
         fprintf(stderr, "Terminating, exit code 1.\n");
-        fclose(fp);
         return 1;
     }
 
     const char s[2] = " ";
     char *token;
+    //Loop through each row in the matrix file
     for (int i = 0; i < numRows; i++) {
     	char values[80];
     	fgets(values, sizeof(values), fp);
-    	token = strtok(values, s);
+    	token = strtok(values, s); //get string in matrix (number)
+    	//Loop through each column in the matrix
         for (int j = 0; j < numCols; j++) {
             if (token != NULL) {
-                matrix[i][j] = atoi(token);
+                matrix[i][j] = atoi(token); //convert string to an interger and save
                 token = strtok(NULL, s);
             } else {
                 matrix[i][j] = 0; // Fill with rest zeros
@@ -55,7 +56,11 @@ void matrixCalculation(int matrixA[1][3], int matrixW[3][5], int matrixB[1][5], 
     }
 }
 
-
+/**
+ * This is the main function for the matrix multiplication program. It reads three input matrix files, performs matrix multiplication and addition, and prints the result.
+ * Input parameters: argc (the number of command-line arguments), argv (an array of strings containing the command-line arguments)
+ * Returns: 0 (success) or 1 (failure)
+**/
 int main(int argc, char *argv[]) {
 
     if(argc != 4){ //Check for 3 files
@@ -69,14 +74,17 @@ int main(int argc, char *argv[]) {
     int B[1][5]; //B is 1x5
     int R[1][5]; //R is of dimension 1x5
     
-    readMatrixFile(argv[1], 1, 3, A); //Read matrix A
-    readMatrixFile(argv[2], 3, 5, W); //Read matrix W
-    readMatrixFile(argv[3], 1, 5, B); //Read matrix B
+    if(readMatrixFile(argv[1], 1, 3, A) == 1)//Read matrix A
+    	return 1; //file not found
+    if(readMatrixFile(argv[2], 3, 5, W) == 1)//Read matrix W
+    	return 1; //file not found
+    if(readMatrixFile(argv[3], 1, 5, B) == 1)//Read matrix B
+    	return 1; //file not found
     
     matrixCalculation(A, W, B, R);
     
     //prints result
-    printf("Result %s * %s + %s = [ ", argv[1], argv[2], argv[3]);
+    printf("Result (%s * %s) + %s = [ ", argv[1], argv[2], argv[3]);
     for (int i = 0; i < 1; i++) {
         for (int j = 0; j < 5; j++) {
             printf("%d ", R[i][j]);
